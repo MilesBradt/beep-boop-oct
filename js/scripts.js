e6e2eaa49832c95c16173097aed938a8e1d2eb53
Miles Bradt <smbradtmichael@gmail.com>
2018-10-26 09:03:40 -0700

    have a for loop that determins if number in an array is 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
 beepBoopArray = [];
 
 function countsTo(stopAt) {
-  if (stopAt <= 0) {
+  if (stopAt < 0) {
     return "I'm sorry, but positive numbers only"
   } else {
     for (var number = 0; number <= stopAt; number++) {
       beepBoopArray.push(number);
     }
   }
+  return replaces0WithBeep(beepBoopArray);
+}
+
+function replaces0WithBeep(number) {
+  var splitNumbers = number.toString().split(",");
+  console.log(splitNumbers);
+  for (var i = 0; i <= splitNumbers.length; i++) {
+    if (splitNumbers[i] == "0") {
+      console.log("Beep!");
+    }
+  }
+  return splitNumbers;
 }
 
 
 
 $(document).ready(function() {
 
-console.log(countsTo(35));
+console.log(countsTo(15));
 console.log(beepBoopArray);
 });
