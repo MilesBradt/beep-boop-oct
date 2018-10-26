6da0aaf3987e1ca70bd3c061160edd5db92ae4aa
Miles Bradt <smbradtmichael@gmail.com>
2018-10-26 10:30:21 -0700

    change any number with a 1 to 'Boop'

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,25 @@ function countsTo(stopAt) {
   return startingArray;
 }
 
-
-  function replaces0WithBeep(number) {
-    var splitNumbers = number.toString().split(",");
-    console.log(splitNumbers);
-    for (var i = 0; i <= splitNumbers.length; i++) {
-      if (splitNumbers[i] === "0") {
-        splitNumbers[i] = "Beep";
-      }
+function replacesWithBeepsAndBoops(number) {
+  var splitNumbers = number.toString().split(",");
+  console.log(splitNumbers);
+  for (var i = 0; i < splitNumbers.length; i++) {
+    if (splitNumbers[i] === "0") {
+      splitNumbers[i] = "Beep";
+    } else if (splitNumbers[i].charAt(0) === "1") {
+      splitNumbers[i] = "Boop!"
     }
-    return splitNumbersArray = splitNumbers.slice();
   }
+  return splitNumbersArray = splitNumbers.slice();
+}
 
 
 $(document).ready(function() {
 
 countsTo(15);
-replaces0WithBeep(startingArray);
+replacesWithBeepsAndBoops(startingArray);
+
 console.log(splitNumbersArray);
 
 
