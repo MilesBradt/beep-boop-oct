d9bda687605293fa1d8d2f715cfba978a19c4bc2
Miles Bradt <smbradtmichael@gmail.com>
2018-10-26 10:05:03 -0700

    change scope to work for adding more functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var beepBoopArray = [];
+var startingArray = [];
 var splitNumbersArray = [];
 
 function countsTo(stopAt) {
@@ -6,29 +6,30 @@ function countsTo(stopAt) {
     return "I'm sorry, but positive numbers only"
   } else {
     for (var number = 0; number <= stopAt; number++) {
-      beepBoopArray.push(number);
+      startingArray.push(number);
     }
   }
-  return replaces0WithBeep(beepBoopArray);
+  return startingArray;
 }
 
-function replaces0WithBeep(number) {
-  var splitNumbers = number.toString().split(",");
-  console.log(splitNumbers);
 
-  for (var i = 0; i <= splitNumbers.length; i++) {
-    if (splitNumbers[i] === "0") {
-      splitNumbers[i] = "Beep";
+  function replaces0WithBeep(number) {
+    var splitNumbers = number.toString().split(",");
+    console.log(splitNumbers);
+    for (var i = 0; i <= splitNumbers.length; i++) {
+      if (splitNumbers[i] === "0") {
+        splitNumbers[i] = "Beep";
+      }
     }
+    return splitNumbersArray = splitNumbers.slice();
   }
-  return splitNumbersArray = splitNumbers.slice();
-}
-
 
 
 $(document).ready(function() {
 
-console.log(countsTo(15));
+countsTo(15);
+replaces0WithBeep(startingArray);
 console.log(splitNumbersArray);
+
 
 });
